0f1b1ee4450401b77728714fddf9c28c368b85e7
Yuta Koshizawa <koher@koherent.org>
2016-03-08 18:39:15 +0900

    Add empty test methods for TensorNN

---
+++ TensorSwiftTests/TensorNNTest.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import TensorSwift
+
+class TensorNNTest: XCTestCase {
+    func testMaxPool() {
+        XCTFail("Unimplemented yet.")
+    }
+    
+    func testConv2d() {
+        XCTFail("Unimplemented yet.")
+    }
+}
