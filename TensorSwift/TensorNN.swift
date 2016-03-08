9bbfda85331d5f25551bfc54be4d753cdf5c6fb5
Yuta Koshizawa <koher@koherent.org>
2016-03-08 18:21:42 +0900

    Add `maxPool` and `conv2d` without implementations

--- TensorSwift/TensorNN.swift
+++ TensorSwift/TensorNN.swift
@@ -11,3 +11,21 @@ extension Tensor {
         return Tensor(shape: shape, elements: elements.map { fmax($0, 0.0) })
     }
 }
+
+extension Tensor {
+    public func maxPool(ksize ksize: [Int], strides: [Int]) -> Tensor { // padding = Same
+        assert(shape.dimensions.count == 4, "`shape.dimensions.count` must be 4: \(shape.dimensions.count)")
+        assert(ksize.count >= 4, "`ksize.count` must be greater than or equal to 4: \(ksize.count)")
+        assert(strides.count >= 4, "`strides.count` must be greater than or equal to 4: \(strides.count)")
+        
+        fatalError("Unimplemented yet.")
+    }
+    
+    public func conv2d(filter filter: Tensor, strides: [Int]) -> Tensor { // padding = Same
+        assert(shape.dimensions.count == 4, "`shape.dimensions.count` must be 4: \(shape.dimensions.count)")
+        assert(filter.shape.dimensions.count == 4, "`filter.shape.dimensions.count` must be 4: \(filter.shape.dimensions.count)")
+        assert(strides.count >= 4, "`strides.count` must be greater than or equal to 4: \(strides.count)")
+
+        fatalError("Unimplemented yet.")
+    }
+}
