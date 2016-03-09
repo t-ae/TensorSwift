b1f0b77dc92602bd706b08ff14b28a1fc1706930
Nisho Matsushita <robonich@gmail.com>
2016-03-09 19:57:35 +0900

    Modify for sentences

--- TensorSwift/TensorNN.swift
+++ TensorSwift/TensorNN.swift
@@ -38,14 +38,10 @@ extension Tensor {
         var elements: [Element] = []
         elements.reserveCapacity(numBatches * numCols * numRows * numChannels)
         
-        var batch = 0
-        var y = 0
-        var x = 0
-        
-        for batch=0; batch<shape.dimensions[0].value; batch+=strides[0] {
+        for batch in 0.stride(to: shape.dimensions[0].value, by: strides[0]) {
             var es: [Element] = Array.init(count: shape.dimensions[3].value, repeatedValue: 0)
-            for y=0-padTop; y+ksize[1]-1<shape.dimensions[1].value+padBottom; y+=strides[1] {
-                for x=0-padLeft; x+ksize[2]-1<shape.dimensions[2].value+padRight; x+=strides[2] {
+            for y in (0-padTop).stride(to: shape.dimensions[1].value+padBottom-ksize[1]+1, by: strides[1]) {
+                for x in (0-padLeft).stride(to: shape.dimensions[2].value+padRight-ksize[2]+1, by: strides[2]) {
                     for j in 0..<ksize[1] {
                         if y+j < 0 || y+j >= shape.dimensions[1].value {
                             continue
@@ -63,6 +59,7 @@ extension Tensor {
                 }
             }
         }
+
         return Tensor(shape: [Dimension(numBatches) ,Dimension(numCols), Dimension(numRows), Dimension(numChannels)], elements: elements)
     }
     
