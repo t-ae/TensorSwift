42caabbf709ea3dcecf3aff9b064ee6cb455ffea
Yuta Koshizawa <koher@koherent.org>
2016-04-08 12:16:13 +0900

    Refine codes

--- MNISTTests/Downloader.swift
+++ MNISTTests/Downloader.swift
@@ -2,23 +2,21 @@ import Foundation
 import zlib
 
 func downloadTestData() -> (images: NSData, labels: NSData) {
+    let baseUrl = "http://yann.lecun.com/exdb/mnist/"
     
-    let sourceURL = "http://yann.lecun.com/exdb/mnist/"
-    
-    let test_images = NSData(contentsOfURL: NSURL(string: sourceURL + "t10k-images-idx3-ubyte.gz")!)!
-    let test_labels = NSData(contentsOfURL: NSURL(string: sourceURL + "t10k-labels-idx1-ubyte.gz")!)!
+    let testImages = NSData(contentsOfURL: NSURL(string: baseUrl)!.URLByAppendingPathComponent("t10k-images-idx3-ubyte.gz"))!
+    let testLabels = NSData(contentsOfURL: NSURL(string: baseUrl)!.URLByAppendingPathComponent("t10k-labels-idx1-ubyte.gz"))!
 
-    return (images: uncompressByGZip(test_images)!, labels: uncompressByGZip(test_labels)!)
+    return (images: ungzip(testImages)!, labels: ungzip(testLabels)!)
 }
 
-
-func uncompressByGZip(source: NSData) -> NSData? {
-    if source.length == 0 {
+private func ungzip(source: NSData) -> NSData? {
+    guard source.length > 0 else {
         return nil
     }
     
     var stream: z_stream = z_stream.init(next_in: UnsafeMutablePointer<Bytef>(source.bytes), avail_in: uint(source.length), total_in: 0, next_out: nil, avail_out: 0, total_out: 0, msg: nil, state: nil, zalloc: nil, zfree: nil, opaque: nil, data_type: 0, adler: 0, reserved: 0)
-    if inflateInit2_(&stream, MAX_WBITS + 32, ZLIB_VERSION, Int32(sizeof(z_stream))) != Z_OK {
+    guard inflateInit2_(&stream, MAX_WBITS + 32, ZLIB_VERSION, Int32(sizeof(z_stream))) == Z_OK else {
         return nil
     }
     
@@ -36,5 +34,5 @@ func uncompressByGZip(source: NSData) -> NSData? {
     }
     
     inflateEnd(&stream)
-    return data
+    return NSData(data: data)
 }
