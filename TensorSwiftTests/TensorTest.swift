482b13859a18c58a20c57e27ed24b1bc5b1e4ce8
araki <araki@qoncept.co.jp>
2016-04-13 10:53:44 +0900

    Add testcase.

--- TensorSwiftTests/TensorTest.swift
+++ TensorSwiftTests/TensorTest.swift
@@ -64,8 +64,30 @@ class TensorTest: XCTestCase {
         do {
             let a = Tensor(shape: [2, 3], elements: [1, 2, 3, 4, 5, 6])
             let b = Tensor(shape: [3, 4], elements: [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18])
-            let r = a.matmul_fast(b)
+            let r = a.matmul(b)
             XCTAssertEqual(r, Tensor(shape: [2, 4], elements: [74, 80, 86, 92, 173, 188, 203, 218]))
+        }
+        do {
+            let a = Tensor(shape: [3, 3], elements: [1, 1, 1, 2, 2, 2, 3, 3, 3])
+            let b = Tensor(shape: [3, 3], elements: [1, 1, 1, 2, 2, 2, 3, 3, 3])
+            let r = a.matmul(b)
+            XCTAssertEqual(r, Tensor(shape: [3, 3], elements: [6, 6, 6, 12, 12, 12, 18, 18, 18]))
+        }
+    }
+    
+    func testMatmulFast(){
+        do {
+            let a = Tensor(shape: [2, 3], elements: [1, 2, 3, 4, 5, 6])
+            let b = Tensor(shape: [3, 4], elements: [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18])
+            let r = a.matmul_fast(b)
+            print(r)
+            XCTAssertEqual(r, Tensor(shape: [2, 4], elements: [74, 80, 86, 92, 173, 188, 203, 218]))
+        }
+        do {
+            let a = Tensor(shape: [3, 3], elements: [1, 1, 1, 2, 2, 2, 3, 3, 3])
+            let b = Tensor(shape: [3, 3], elements: [1, 1, 1, 2, 2, 2, 3, 3, 3])
+            let r = a.matmul_fast(b)
+            XCTAssertEqual(r, Tensor(shape: [3, 3], elements: [6, 6, 6, 12, 12, 12, 18, 18, 18]))
         }
     }
     
