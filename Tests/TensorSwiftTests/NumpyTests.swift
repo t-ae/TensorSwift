95be363ed1395dd5d75e45dbc387eb5b68c8fbe0
araki <araki@qoncept.co.jp>
2016-10-24 20:08:23 +0900

    Embed npy test datas into NumpyTests

--- Tests/TensorSwiftTests/NumpyTests.swift
+++ Tests/TensorSwiftTests/NumpyTests.swift
@@ -7,7 +7,8 @@ class NumpyTests: XCTestCase {
 
     func testLoadNpyA() {
         
-        guard let x = load("a") else {
+        
+        guard let x = Tensor(npyData: NpyA) else {
             XCTFail()
             return
         }
@@ -18,7 +19,7 @@ class NumpyTests: XCTestCase {
     
     func testLoadNpyB() {
         
-        guard let x = load("b") else {
+        guard let x = Tensor(npyData: NpyB) else {
             XCTFail()
             return
         }
@@ -29,7 +30,7 @@ class NumpyTests: XCTestCase {
     
     func testLoadNpyC() {
         
-        guard let x = load("c") else {
+        guard let x = Tensor(npyData: NpyC) else {
             XCTFail()
             return
         }
@@ -40,7 +41,7 @@ class NumpyTests: XCTestCase {
     
     func testLoadNpyD() {
         
-        guard let x = load("d") else {
+        guard let x = Tensor(npyData: NpyD) else {
             XCTFail()
             return
         }
@@ -51,7 +52,7 @@ class NumpyTests: XCTestCase {
     
     func testLoadNpyE() {
     
-        guard let x = load("e") else {
+        guard let x = Tensor(npyData: NpyE) else {
             XCTFail()
             return
         }
@@ -60,15 +61,69 @@ class NumpyTests: XCTestCase {
         XCTAssertEqual(x.elements, [1.0, 2.0, 3.0, 4.0, 5.0, 6.0])
     }
     
-    func load(_ fileName: String) -> Tensor? {
-        guard let url = Bundle(for: type(of: self)).url(forResource: fileName, withExtension: "npy") else {
-            return nil
-        }
-        
-        guard let data = try? Data(contentsOf: url) else {
-            return nil
-        }
-        
-        return Tensor(npyData: data)
+    static var allTests : [(String, (NumpyTests) -> () throws -> Void)] {
+        return [
+            ("testLoadNpyA", testLoadNpyA),
+            ("testLoadNpyB", testLoadNpyB),
+            ("testLoadNpyC", testLoadNpyC),
+            ("testLoadNpyD", testLoadNpyD),
+            ("testLoadNpyE", testLoadNpyE),
+        ]
     }
+
 }
+
+fileprivate let NpyA = Data(bytes:
+    [UInt8](arrayLiteral:147, 78,85,77,80,89, 1, 0,70, 0,123,39,100,101,115,99,
+            114,39,58,32,39,60,102,52,39,44,32,39,102,111,114,116,
+            114,97,110,95,111,114,100,101,114,39,58,32,70,97,108,115,
+            101,44,32,39,115,104,97,112,101,39,58,32,40,51,44,41,
+            44,32,125,32,32,32,32,32,32,32,32,32,32,32,32,10,
+            0,0,128,63, 0, 0, 0,64, 0, 0,64,64
+    )
+)
+
+fileprivate let NpyB = Data(bytes:
+    [UInt8](arrayLiteral: 147, 78, 85, 77, 80, 89,  1,  0, 70,  0,123, 39,100,101,115, 99,
+            114, 39, 58, 32, 39, 60,102, 56, 39, 44, 32, 39,102,111,114,116,
+            114, 97,110, 95,111,114,100,101,114, 39, 58, 32, 70, 97,108,115,
+            101, 44, 32, 39,115,104, 97,112,101, 39, 58, 32, 40, 50, 44, 32,
+            50, 41, 44, 32,125, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 10,
+            0,  0,  0,  0,  0,  0,240, 63,  0,  0,  0,  0,  0,  0,  0, 64,
+            0,  0,  0,  0,  0,  0,  8, 64,  0,  0,  0,  0,  0,  0, 16, 64
+    )
+)
+
+
+fileprivate let NpyC = Data(bytes:
+    [UInt8](arrayLiteral: 147, 78, 85, 77, 80, 89,  1,  0, 70,  0,123, 39,100,101,115, 99,
+            114, 39, 58, 32, 39, 60,102, 52, 39, 44, 32, 39,102,111,114,116,
+            114, 97,110, 95,111,114,100,101,114, 39, 58, 32, 70, 97,108,115,
+            101, 44, 32, 39,115,104, 97,112,101, 39, 58, 32, 40, 50, 44, 32,
+            50, 44, 32, 50, 41, 44, 32,125, 32, 32, 32, 32, 32, 32, 32, 10,
+            0,  0,128, 63,  0,  0,  0, 64,  0,  0, 64, 64,  0,  0,128, 64,
+            0,  0,160, 64,  0,  0,192, 64,  0,  0,224, 64,  0,  0,  0, 65
+    )
+)
+
+fileprivate let NpyD = Data(bytes:
+    [UInt8](arrayLiteral: 147, 78, 85, 77, 80, 89,  1,  0, 70,  0,123, 39,100,101,115, 99,
+            114, 39, 58, 32, 39, 60,102, 56, 39, 44, 32, 39,102,111,114,116,
+            114, 97,110, 95,111,114,100,101,114, 39, 58, 32, 70, 97,108,115,
+            101, 44, 32, 39,115,104, 97,112,101, 39, 58, 32, 40, 52, 44, 41,
+            44, 32,125, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 10,
+            0,  0,  0,  0,  0,  0,240, 63,  0,  0,  0,  0,  0,  0,224, 63,
+            0,  0,  0,  0,  0,  0,208, 63,  0,  0,  0,  0,  0,  0,192, 63
+    )
+)
+
+fileprivate let NpyE = Data(bytes:
+    [UInt8](arrayLiteral: 147, 78, 85, 77, 80, 89,  1,  0, 70,  0,123, 39,100,101,115, 99,
+            114, 39, 58, 32, 39, 60,102, 52, 39, 44, 32, 39,102,111,114,116,
+            114, 97,110, 95,111,114,100,101,114, 39, 58, 32, 70, 97,108,115,
+            101, 44, 32, 39,115,104, 97,112,101, 39, 58, 32, 40, 50, 44, 32,
+            51, 41, 44, 32,125, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 10,
+            0,  0,128, 63,  0,  0,  0, 64,  0,  0, 64, 64,  0,  0,128, 64,
+            0,  0,160, 64,  0,  0,192, 64
+    )
+)
